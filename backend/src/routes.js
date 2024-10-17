a35f6fe69ad1b1e11355bc6796d8e5d86bf21639
Lucas Rendo <lucasrendo1@gmail.com>
2024-10-17 13:35:26 -0300

    Create endpoint routes and attach logic

--- backend/src/routes.js
+++ backend/src/routes.js
@@ -1,5 +1,9 @@
 import express from 'express';
+import { getCountries, getCountry } from './controller';
 
 const router = express.Router();
+
+router.get('/', getCountries);
+router.get('/country', getCountry);
 
 export default router;
