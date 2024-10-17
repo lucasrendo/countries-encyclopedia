5841a222c04d05082ac33fb86c8a7350f2fe2cc3
Lucas Rendo <lucasrendo1@gmail.com>
2024-10-17 18:23:28 -0300

    Add cors headers to allow cross origin requests

--- backend/src/index.js
+++ backend/src/index.js
@@ -1,6 +1,7 @@
 import 'dotenv/config';
 import express from 'express';
 import routes from './routes';
+import cors from 'cors';
 
 if (!process.env.PORT) {
     console.error('Please set PORT environment variable');
@@ -11,6 +12,7 @@ const port = process.env.PORT;
 
 const app = express();
 app.set('json spaces', 2);
+app.use(cors());
 app.use(express.json());
 
 app.listen(port, () => {
