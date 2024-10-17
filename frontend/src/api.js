2a47018d61ea5dce2f54315fa276a6b1c0743a8e
Lucas Rendo <lucasrendo1@gmail.com>
2024-10-17 15:06:32 -0300

    create connection with API

---
+++ frontend/src/api.js
@@ -0,0 +1,25 @@
+const baseUrl = process.env.REACT_APP_API_URL;
+
+export const getCountries = async () => {
+    try {
+        const response = await fetch(`${baseUrl}/`);
+        const data = await response.json();
+
+        return data;
+    } catch (e) {
+        console.log(e);
+        return e;
+    }
+};
+
+export const getCountry = async (name, code) => {
+    try {
+        const response = await fetch(`${baseUrl}/country?name=${name}&code=${code}`);
+        const data = await response.json();
+
+        return data;
+    } catch (e) {
+        console.log(e);
+        return e;
+    }
+};
