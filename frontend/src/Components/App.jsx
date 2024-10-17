5dc387762d9388e277e18a5fa675ae2e7ffa431c
Lucas Rendo <lucasrendo1@gmail.com>
2024-10-17 20:19:39 -0300

    add global css

--- frontend/src/Components/App.jsx
+++ frontend/src/Components/App.jsx
@@ -2,6 +2,7 @@ import { useEffect, useState } from 'react';
 import { BrowserRouter as Router, Route, Routes, Navigate } from 'react-router-dom';
 import List from './List/List';
 import Country from './Country/Country';
+import './global.css';
 
 function App() {
     return (
